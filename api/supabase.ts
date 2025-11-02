de4be73dd792d49c74fd2453f864dd9c0a07aa28
Kyrylo Kapinos <kapinos.kirill.99@gmail.com>
2025-11-02 19:41:24 +0100

    Supabase + Google auth setup

---
+++ api/supabase.ts
@@ -0,0 +1,21 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../src/lib/database.types";
+
+import "react-native-url-polyfill/auto";
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_PUBLIC_KEY!;
+
+// Create a Supabase client with persistent session storage
+export const supabase = createClient<Database>(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  }
+);
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { supabase } from "@/api/supabase";
 import {
   UIButton,
   UICard,
@@ -6,6 +7,11 @@ import {
   UIText,
 } from "@/components/ui";
 import { Ionicons } from "@expo/vector-icons";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
 import { FlatList, TouchableOpacity, View } from "react-native";
@@ -71,6 +77,45 @@ type Filter = {
   location: string;
 };
 
+export const GoogleButton = () => {
+  GoogleSignin.configure({
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+    webClientId: "YOUR CLIENT ID FROM GOOGLE CONSOLE",
+  });
+
+  return (
+    <GoogleSigninButton
+      size={GoogleSigninButton.Size.Wide}
+      color={GoogleSigninButton.Color.Dark}
+      onPress={async () => {
+        try {
+          await GoogleSignin.hasPlayServices();
+          const userInfo = await GoogleSignin.signIn();
+          if (userInfo?.data?.idToken) {
+            const { data, error } = await supabase.auth.signInWithIdToken({
+              provider: "google",
+              token: userInfo.data.idToken,
+            });
+            console.log(error, data);
+          } else {
+            throw new Error("no ID token present!");
+          }
+        } catch (error: any) {
+          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            // user cancelled the login flow
+          } else if (error.code === statusCodes.IN_PROGRESS) {
+            // operation (e.g. sign in) is in progress already
+          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            // play services not available or outdated
+          } else {
+            // some other error happened
+          }
+        }
+      }}
+    />
+  );
+};
+
 export default function SearchScreen() {
   const { theme, rt } = useUnistyles();
   const styles = stylesheet;
@@ -120,6 +165,7 @@ export default function SearchScreen() {
 
   return (
     <SafeAreaView style={styles.safeArea}>
+      <GoogleButton />
       <UIContainer>
         <View style={styles.header}>
           <View style={styles.searchContainer}>
